660ee2b05262468f912bd1cd58581abb1204f8ba
Sarah Hightower <sightower@gmail.com>
2023-01-26 09:36:14 -0800

    user profile fix

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import AuthForm from '../features/auth/AuthForm';
 import Home from '../features/home/Home';
 import { me } from './store';
+import UserProfile from '../features/userProfile/userProfile';
 
 /**
  * COMPONENT
@@ -23,6 +25,7 @@ const AppRoutes = () => {
         <Routes>
           <Route path="/*" element={<Home />} />
           <Route to="/home" element={<Home />} />
+          <Route path="/profile" element={<UserProfile />} />
         </Routes>
       ) : (
         <Routes>
@@ -38,6 +41,7 @@ const AppRoutes = () => {
             path="/signup"
             element={<AuthForm name="signup" displayName="Sign Up" />}
           />
+          <Route path="/profile" element={<UserProfile />} />
         </Routes>
       )}
     </div>
--- client/features/userProfile/userProfile.js
+++ client/features/userProfile/userProfile.js
@@ -1,19 +1,22 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { getSingleProfile } from "../../slices/userProfileSlice";
 // import PropTypes from 'prop-types'
 
 
 
-function userProfile() {
+export const UserProfile = () => {
+
   return (
-    <div id="userProfile">userProfile
+    <div id="userProfile">
        {/* <div id="single-product-info" style={singleProductStyle}>
                 {singleUser.isAdmin ? (
                     <EditProductForm />
                 ) : ( */}
           <div id="profileContainer">
-            <img src={`${imageUrl}`} height="400px" />
+            <img src="imageUrl" height="400px" />
             <section id="detailsContainer">
                 <h1>firstName</h1>
                 <h2>lastName</h2>
@@ -23,11 +26,11 @@ function userProfile() {
                   <p>Details: description?</p>
                 <form id="messageFriend">Message</form> 
                 <button id="addFriend">Add Friend</button>
-                <h2 id="rating">Rating: {rating} </h2>
+                <h2 id="rating">Rating: rating </h2>
               </section>
               <section id="statusContainer">
-                  <input id="statusPhoto">{imageAttachment}</input>
-                  <input id="statusText">Status: "Saved a puppy today! #PETA #Saint #GoingtoHeaven"</input>
+                  <input id="statusPhoto" placeholder="imageAttachment"></input>
+                  <input id="statusText" placeholder="Status: 'Saved a puppy today! #PETA #Saint #GoingtoHeaven'"></input>
               </section>
           </div>
           {/* )}
@@ -38,4 +41,4 @@ function userProfile() {
 
 // userProfile.propTypes = {}
 
-export default userProfile
+export default UserProfile;
