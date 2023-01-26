9a298ebf8ba8b11cee9587154991c50de0faaa21
David Zeng <dzeng91@gmail.com>
2023-01-26 00:41:14 -0500

    just added some more into userProfile

--- client/components/userProfile.js
+++ client/components/userProfile.js
@@ -3,23 +3,35 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 // import PropTypes from 'prop-types'
 
+
+
 function userProfile() {
   return (
     <div id="userProfile">userProfile
-      <div id="profileContainer">
-        <img src={`${imageUrl}`} height="400px" />
-        <section id="colorContainer">
-            <h1>firstName</h1>
-            <h2>lastName</h2>
-              <section id="userInfoContainer">
-              <h3>Location: city, state</h3>
+       {/* <div id="single-product-info" style={singleProductStyle}>
+                {singleUser.isAdmin ? (
+                    <EditProductForm />
+                ) : ( */}
+          <div id="profileContainer">
+            <img src={`${imageUrl}`} height="400px" />
+            <section id="detailsContainer">
+                <h1>firstName</h1>
+                <h2>lastName</h2>
+                  <section id="userInfoContainer">
+                    <h3>Location: city, state</h3>
+                  </section>
+                  <p>Details: description?</p>
+                <form id="messageFriend">Message</form> 
+                <button id="addFriend">Add Friend</button>
+                <h2 id="rating">Rating: {rating} </h2>
               </section>
-              <p>Details: description?</p>
-            <section id="priceColorContainer">
-                <h3>Price: $ {price}</h3>
-            </section>
-          </section>
-      </div>
+              <section id="statusContainer">
+                  <input id="statusPhoto">{imageAttachment}</input>
+                  <input id="statusText">Status: "Saved a puppy today! #PETA #Saint #GoingtoHeaven"</input>
+              </section>
+          </div>
+          {/* )}
+        </div> */}
     </div>
   )
 }
