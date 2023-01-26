360a5c791a5a6283b3e3f9e5abb0eec50ef5d598
Sarah Hightower <sightower@gmail.com>
2023-01-25 20:43:40 -0800

    Navbar + Sidebar with responsiveness

---
+++ client/features/sidebar/Sidebar.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Sidebar() {
+return (
+  <>
+    <div className="flex border-2 border-[#E68584] justify-center md:float-right shadow-xl shadow-rose-200 m-8 rounded-xl">
+      <div className="flex flex-col h-64 overflow:auto md:h-screen p-3 w-auto">
+       <div className="space-y-3 items-center">
+        <div className="flex flex-col items-center md:p-4">
+         <h2 className="text-xl text-[#E68584] font-bold font-serif">YOUR RATING:</h2>
+         <p className="text-amber-400">★ ★ ★ ★ ★</p>
+         <Link className="text-[#E68584] mt-10">Fellows (0)</Link>
+        </div>
+       </div>
+      </div>
+    </div>
+  </>
+);
+}
