011deb25e6ad29095bf78e99654363d294718808
Sarah Hightower <sightower@gmail.com>
2023-01-27 12:34:40 -0800

    profile, links, navbar edits

---
+++ client/features/VIP/Locked.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Locked = () => {
+    return(
+        <div className=" flex flex-col items-center bg-[#EBAF4C] p-5 m-10 rounded-md">
+            <h1 className="text-white text-3xl font-bold text-center">You must have a rating of 4 stars or more to see this page.</h1>
+            <h2 className="text-white text-3xl">Want to boost your rating?</h2>
+            <Link to="/boost" className="text-white text-3xl font-bold underline">Click here!</Link>
+    </div>
+    );
+};
+
+export default Locked;
--- client/features/navbar/Navbar.jsx
+++ client/features/navbar/Navbar.jsx
@@ -1,21 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import DropdownMenu from "./Dropdown.jsx";
 
 
 const Navbar = () => {
   return (
     <div className="border-b-2 border-[#E68584] w-full">
       <div className="container flex flex-wrap items-center justify-between w-auto">
-        <img src="https://i.ibb.co/FXJrqMt/epitome-1.png" className="w-auto h-auto ml-5 mb-2 mt-1"></img>
+        <Link to="/"> <img src="https://i.ibb.co/FXJrqMt/epitome-1.png" className="w-auto h-auto ml-5 mb-2 mt-1"></img> </Link>
         <div className="flex flex-row w-full sm:block sm:w-auto">
           <div className="flex flex-row flex-wrap items-center justify-center p-4 mt-4 rounded-lg sm:flex-row sm:space-x-8 sm:mt-0 sm:text-sm sm:font-medium sm:border-0 sm:bg-white">
             <Link to="/create" className="font-bold block py-2 pl-3 pr-4 text-[#E68584] rounded hover:bg-[#E68584] sm:hover:bg-transparent sm:border-0 hover:text-white sm:p-0 md:hover:text-rose-200" aria-current="page">+</Link>
             <Link to="/home" className="block py-2 pl-3 pr-4 text-[#E68584] rounded hover:bg-[#E68584] sm:hover:bg-transparent sm:border-0 hover:text-white sm:p-0 md:hover:text-rose-200" aria-current="page">Home</Link>
-            <Link to="/home" className="block py-2 pl-3 pr-4 text-[#E68584] rounded hover:bg-[#E68584] sm:hover:bg-transparent sm:border-0 hover:text-white sm:p-0 md:hover:text-rose-200" aria-current="page">Messages</Link>
-            <Link to="/home" className="block py-2 pl-3 pr-4 text-[#E68584] rounded hover:bg-[#E68584] sm:hover:bg-transparent sm:border-0 hover:text-white sm:p-0 md:hover:text-rose-200">Notifications</Link>
-            <Link to="/home" className="block py-2 pl-3 pr-4 text-[#E68584] rounded hover:bg-[#E68584] sm:hover:bg-transparent sm:border-0 hover:text-white sm:p-0 md:hover:text-rose-200" aria-current="page">VIP</Link>
-            <Link to="/profile" className="block py-2 pl-3 pr-4 text-[#E68584] rounded hover:bg-[#E68584] sm:hover:bg-transparent sm:border-0 hover:text-white sm:p-0 md:hover:text-rose-200" aria-current="page">Profile</Link>
+            <Link to="/messages" className="block py-2 pl-3 pr-4 text-[#E68584] rounded hover:bg-[#E68584] sm:hover:bg-transparent sm:border-0 hover:text-white sm:p-0 md:hover:text-rose-200" aria-current="page">Messages</Link>
+            <Link to="/VIP" className="block py-2 pl-3 pr-4 text-[#E68584] rounded hover:bg-[#E68584] sm:hover:bg-transparent sm:border-0 hover:text-white sm:p-0 md:hover:text-rose-200" aria-current="page">VIP</Link>
+            <DropdownMenu />
           </div>
         </div>
       </div>
