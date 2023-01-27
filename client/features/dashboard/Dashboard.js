fc893e0cfe42dc56a74d80c4d4d84fec5f75c08f
GMV44 <root@LAPTOP-B6VQPMKU.localdomain>
2023-01-27 16:05:23 -0600

    mapping posts

--- client/features/dashboard/Dashboard.js
+++ client/features/dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Home from "../home/Home";
 
 export const Dashboard = () => {
     return(
         <div>
             <h1 className="text-3xl font-bold text-[#E68584] m-10">Dashboard</h1>
+            <Home/>
         </div>
     )
 }
