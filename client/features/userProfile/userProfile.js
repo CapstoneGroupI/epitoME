0a892ba56bbc6535d24310d597b47e034e3be4c2
Sarah Hightower <sightower@gmail.com>
2023-01-31 13:58:19 -0800

    searchbar functionality

--- client/features/userProfile/userProfile.js
+++ client/features/userProfile/userProfile.js
@@ -17,14 +17,16 @@ export const UserProfile = (props) => {
   const pronouns = useSelector((state) => state.auth.me.pronouns);
   const birthday = useSelector((state) => state.auth.me.birthday);
   const profilePic = useSelector((state) => state.auth.me.profilePic);
+  const username = useSelector((state) => state.auth.me.username);
 
   return (
     <div id="userProfile">
-          <div id="profileContainer" className="flex flex-row bg-[white] w-3/5 m-10 rounded-md shadow-lg shadow-[#EBAF4C] p-4 relative">
+          <div id="profileContainer" className="flex flex-col md:flex-row bg-[white] w-3/5 m-10 rounded-md shadow-lg shadow-[#EBAF4C] p-4 relative">
             <img src={profilePic} className="h-72 w-72 rounded-full border-2 border-[#EBAF4C] shadow-lg shadow-stone" />
             <div id="detailsContainer" className="flex flex-col ml-5">
                 <h1 className="text-[#EBAF4C] text-3xl font-bold">{firstName} {lastName} <span className="text-[#EBAF4C] text-sm"> {pronouns}</span></h1>
-          <div id="userInfoContainer">
+          <div id="userInfoContainer" className="flex flex-col">
+          <h3 className="text-[#EBAF4C] text-2xl">@{username}</h3>
             <h3 className="text-[#EBAF4C] text-2xl">{city}, {state}</h3>
             <h2 className="text-[#EBAF4C] text-2xl"> {birthday}</h2>
             <p>{bio}</p>
