566bb51d4f9c87bc9e2d9494b7ff019b0aff5708
GMV44 <root@LAPTOP-B6VQPMKU.localdomain>
2023-02-03 17:01:57 -0600

    small change

--- client/features/settings/Email.js
+++ client/features/settings/Email.js
@@ -15,7 +15,7 @@ const Email = () => {
             <input className=" border-b-2 border-honey w-2/5 mt-4 m-2 p-2" placeholder="enter new email..."></input>
             <button className="bg-honey text-white text-center rounded w-1/5 pt-2 py-2 m-2 ml-4  mt-4 font-bold hover:bg-white hover:text-honey hover:border-2 hover:border-honey">Save New Email</button>
             <div className="flex flex-row"><Checkbox defaultChecked /> <h4 className="text-stone-500 text-sm pt-2">Receive notifications at this email?</h4></div>
-            <p className="text-stone-300 w-3/5 m-2">*In order to confirm this new email, you must verify using the link sent to the email address you provided. You will receive a verification email shortly. If you don't receive an email within an hour, <Link to="/contact-us" className="underline hover:text-honey"> click here. </Link>*</p>
+            <p className="text-stone-300 w-3/5 m-2">*In order to confirm this new email, you must verify using the link sent to the email address you provided. You will receive a verification email shortly. If you don't receive an email within an hour, <Link to="/contact" className="underline hover:text-honey"> click here. </Link>*</p>
         </div>
     </div>
     );
