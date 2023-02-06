8af5b4cbe0948c40f3085180b703cc2bda189805
GMV44 <root@LAPTOP-B6VQPMKU.localdomain>
2023-02-06 16:42:25 -0600

    star functionality

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -8,10 +8,9 @@ import { updatePostAsync } from "../../slices/singlePostSlice";
 /**
  * COMPONENT
  */
-const Home = ({ userId, isLoggedIn, props}) => {
+const Home = ({ userId, isLoggedIn, props }) => {
   const username = useSelector((state) => state.auth.me.username);
   const firstName = useSelector((state) => state.auth.me.firstName);
-
 
   const posts = useSelector(selectPosts);
 
@@ -19,20 +18,25 @@ const Home = ({ userId, isLoggedIn, props}) => {
 
   useEffect(() => {
     dispatch(getAllPostsAsync());
-    
-  }, [userId,]);
+  }, []);
 
-  const handleUpdate = (id,rating) => {
-    console.log('this is rating --------------',id, rating)
-    dispatch(updatePostAsync({id,rating})) 
-  }
+  const handleUpdate = (id, rating) => {
+    console.log("this is rating --------------", id, rating);
+    dispatch(updatePostAsync({ id, rating }));
+  };
+
+  const [hoverValues, setHoverValues] = useState([]);
+
+  useEffect(() => {
+    setHoverValues(posts.map(() => undefined));
+  }, [posts]);
 
   //npx tailwindcss -i ./public/style.css -o ./public/output.css --watch
 
   return (
-    <div className=""> 
+    <div className="">
       <div className="">
-        {posts.map((post) => {
+        {posts.map((post, index) => {
           let date = new Date(post.createdAt);
           let options = {
             year: "numeric",
@@ -45,12 +49,34 @@ const Home = ({ userId, isLoggedIn, props}) => {
             const total = post.rating.reduce((acc, c) => acc + c, 0);
             return total / post.rating.length;
           }
-          
+
           const average = getAvg(post);
-          
+          const stars = Array(5).fill(0);
+
           let formattedDate = date.toLocaleDateString("en-US", options);
+
+          const handleMouseOver = (value) => {
+            setHoverValues((prev) => {
+              let newHoverValues = [...prev];
+              newHoverValues[index] = value;
+              return newHoverValues;
+            });
+          };
+
+          const handleMouseLeave = (value) => {
+            setHoverValues((prev) => {
+              let newHoverValues = [...prev];
+              newHoverValues[index] = undefined;
+              return newHoverValues;
+            });
+          };
+
           return (
-            <div className="flex flex-col mx-auto w-3/5 max-w-xl m-10 bg-[#E68584] rounded-md shadow-lg shadow-[#EBAF4C]">
+            <div className="w-4/6 mx-auto">
+            <div
+              key={post.id}
+              className="relative flex flex-col mx-auto w-4-6 max-w-lg m-10 bg-[#fff6f6] border-2 border-[#E68584] rounded-md shadow-lg shadow-[#913c3b]"
+            >
               <div className="flex flex-row items-center justify-around">
                 <div className="flex flex-row items-center flex-wrap">
                   <img
@@ -58,70 +84,87 @@ const Home = ({ userId, isLoggedIn, props}) => {
                     src={post.user.profilePic}
                   />
                   <div className=" m-2">
-                    <h1 className="font-bold font-roboto text-amber-300">
+                    <h1 className="font-bold  text-[#E68584]">
                       {post.user.firstName} {post.user.lastName}
                     </h1>
-                    <h1 className="font-bold  text-white text-sm">
-                      @{post.user.username}
+                    <h1 className="font-bold  text-black text-sm">
+                      @ {post.user.username}
                     </h1>
                   </div>
                 </div>
                 <div>
-                  <p className="font-bold font-serif text-xs m-2 text-white">
+                  <h3 className="font-bold text-sm m-2 text-black">
                     {formattedDate}
-                  </p>
+                  </h3>
                 </div>
               </div>
-              <div className="w-10/12 h-0.5 mx-auto rounded-lg bg-amber-300" />
+              <div className="w-10/12 h-0.5 mx-auto rounded-lg bg-[#E68584]" />
               <div className="mx-auto m-5 w-10/12">{post.text}</div>
               <img
                 className="rounded mx-auto m-5 w-10/12"
                 src={post.image}
               ></img>
               <div className="flex justify-end p-5">
-              <div className="">{average.toFixed(2)}</div>
+                <div className=" font-bold text-[#E68584]">{average.toFixed(2)}</div>
               </div>
-              
-              <div className="h-10 bg-amber-300 flex items-center justify-around">
+
+              <div className="h-10 bg-[#E68584] flex items-center justify-around">
                 <div className="flex justify-around w-2/12">
-                  
-                  <button onClick={() => handleUpdate(post.id, 1)}><FaStar className="hover:text-[#E68584]"/></button>
-                  <button onClick={() => handleUpdate(post.id, 2)}><FaStar className="hover:text-[#E68584]"/></button>
-                  <button onClick={() => handleUpdate(post.id, 3)}><FaStar className="hover:text-[#E68584]"/></button>
-                  <button onClick={() => handleUpdate(post.id, 4)}><FaStar className="hover:text-[#E68584]"/></button>
-                  <button onClick={() => handleUpdate(post.id, 5)}><FaStar className="hover:text-[#E68584]"/></button>
-                  
+                  {stars.map((_, starIndex) => {
+                    let ratingValue = starIndex + 1;
+
+                    return (
+                      <FaStar
+                        key={starIndex}
+                        className="h-5 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:cursor-pointer"
+                        value={ratingValue}
+                        color={
+                          hoverValues[index] > starIndex ? "#ffc107" : "black"
+                        }
+                        onClick={() => handleUpdate(post.id, ratingValue)}
+                        onMouseOver={() => handleMouseOver(ratingValue)}
+                        onMouseLeave={() => handleMouseLeave()}
+                      />
+                    );
+                  })}
                 </div>
-                <button className="">Comments</button>
+                <button className="font-bold text-amber-300">Comments</button>
               </div>
+            </div>
             </div>
           );
         })}
       </div>
     </div>
 
-// {[...Array(5)].map((star, i) => {
-//   const ratingValue = i + 1;
+    // {[...Array(5)].map((star, i) => {
+    //   const ratingValue = i + 1;
 
-//   return (
-//     <label>
-//       <input
-//         className=" hidden "
-//         type="radio"
-//         name="rating"
-//         value={ratingValue}
-//         onClick={() => dispatch(updatePostAsync(post.id, ratingValue))}
-        
-//       />
-//       <FaStar
-//         color={
-//           ratingValue <= post.rating ? "#ffc107" : "#e4e5e9"
-//         }
-//         className=" cursor-pointer"
-//       />
-//     </label>
-//   );
-// })}
+    //   return (
+    //     <label>
+    //       <input
+    //         className=" hidden "
+    //         type="radio"
+    //         name="rating"
+    //         value={ratingValue}
+    //         onClick={() => dispatch(updatePostAsync(post.id, ratingValue))}
+
+    //       />
+    //       <FaStar
+    //         color={
+    //           ratingValue <= post.rating ? "#ffc107" : "#e4e5e9"
+    //         }
+    //         className=" cursor-pointer"
+    //       />
+    //     </label>
+    //   );
+    // })}
+
+    // {/* <button onClick={() => handleUpdate(post.id, 1)}><FaStar className="hover:text-amber-300 "/></button>
+    // <button onClick={() => handleUpdate(post.id, 2)}><FaStar className="hover:text-amber-300"/></button>
+    // <button onClick={() => handleUpdate(post.id, 3)}><FaStar className="hover:text-amber-300"/></button>
+    // <button onClick={() => handleUpdate(post.id, 4)}><FaStar className="hover:text-amber-300"/></button>
+    // <button onClick={() => handleUpdate(post.id, 5)}><FaStar className="hover:text-amber-300"/></button> */}
   );
 };
 
