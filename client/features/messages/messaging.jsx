67539324e90096270df4487ba6a527541b5e7587
DJ <ca12c4@gmail.com>
2023-02-14 12:42:37 -0500

    message fixes

--- client/features/messages/messaging.jsx
+++ client/features/messages/messaging.jsx
@@ -56,7 +56,9 @@ const Messaging = ({ clickedUser }) => {
           <div key={message.id} id="message" className="p-3 flex gap-5 mb-2.5">
             <div id="messageInfo" className="flex flex-col color-grey ">
               <img className="h-10 w-10 rounded-3xl object-cover" src={message.user.profilePic} alt="" />
-              <span>just now</span>
+              
+              {/* <span>{new Date(message.timestamp).toLocaleString()}</span> */}
+              <span>{message.user.firstName}</span>
             </div>
             <div id="messagecontent" className="flex felx-col max-w-[80%] gap-2.5" >
               <p id="text" className="bg-[pink] pt-2.5 pb-2.5 pr-5 pl-5 rounded ">{message.text}</p>
