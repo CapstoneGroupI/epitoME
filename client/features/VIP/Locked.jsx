04054e4dcc7affcf7d6dda5e39bd6dfd55a5b767
DJ <ca12c4@gmail.com>
2023-02-14 13:13:37 -0500

    output.css

--- client/features/VIP/Locked.jsx
+++ client/features/VIP/Locked.jsx
@@ -6,10 +6,12 @@ import Footer from "../footer/Footer";
 const Locked = () => {
     return(
         <>
-        <div className=" flex flex-col items-center bg-[#EBAF4C] p-5 m-10 rounded-md">
+            <div className= "h-screen">
+        <div className="flex flex-col items-center bg-[#EBAF4C] p-5 m-10 rounded-md">
             <h1 className="text-white text-3xl font-bold text-center">You must have a rating of 4 stars or more to see this page.</h1>
             <h2 className="text-white text-3xl">Just a regular pleb? Want to boost your rating?</h2>
             <Link to="/boost" className="text-white text-3xl font-bold underline">Click here!</Link>
+    </div>
     </div>
     <div> < Footer /> </div>
     </>
