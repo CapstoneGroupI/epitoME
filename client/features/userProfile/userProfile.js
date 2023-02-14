f4a36974e50314073b0ec5041e425ddd289e47d1
DJ <ca12c4@gmail.com>
2023-02-14 15:08:19 -0500

    single profile fixes

--- client/features/userProfile/userProfile.js
+++ client/features/userProfile/userProfile.js
@@ -4,22 +4,29 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getSingleProfile } from "../../slices/userProfileSlice";
 import { createFellow } from "../../slices/fellowsSlice";
+import { useParams } from "react-router-dom";
+import { selectSingleUser } from "../../slices/userProfileSlice";
 // import PropTypes from 'prop-types'
 
 
-export const UserProfile = (props) => {
-  const firstName = useSelector((state) => state.auth.me.firstName);
-  const lastName = useSelector((state) => state.auth.me.lastName);
-  const bio = useSelector((state) => state.auth.me.description);
-  const city = useSelector((state) => state.auth.me.city);
-  const state = useSelector((state) => state.auth.me.state);
-  const rating = useSelector((state) => state.auth.me.rating);
-  const pronouns = useSelector((state) => state.auth.me.pronouns);
-  const birthday = useSelector((state) => state.auth.me.birthday);
-  const profilePic = useSelector((state) => state.auth.me.profilePic);
-  const username = useSelector((state) => state.auth.me.username);
+export const UserProfile = () => {
+  // const firstName = useSelector((state) => state.auth.me.firstName);
+  // const lastName = useSelector((state) => state.auth.me.lastName);
+  // const bio = useSelector((state) => state.auth.me.description);
+  // const city = useSelector((state) => state.auth.me.city);
+  // const state = useSelector((state) => state.auth.me.state);
+  // const rating = useSelector((state) => state.auth.me.rating);
+  // const pronouns = useSelector((state) => state.auth.me.pronouns);
+  // const birthday = useSelector((state) => state.auth.me.birthday);
+  // const profilePic = useSelector((state) => state.auth.me.profilePic);
+  // const username = useSelector((state) => state.auth.me.username);
   const isLoggedIn = useSelector((state) => !!state.auth.me.userId); // id or userId ??
   const userId = useSelector((state) => state.auth.me.userId); // id or userId ??
+
+  const params = useParams()
+
+  const users = useSelector(selectSingleUser)
+  console.log("this is the user", users[0]?.username)
 
   const dispatch = useDispatch();
 
@@ -29,18 +36,22 @@ export const UserProfile = (props) => {
     }
   }
 
+  useEffect(() => {
+    dispatch(getSingleProfile(params))
+   },[])
+
   return (
-    <div id="userProfile">
+    <div key={users[0]} id="userProfile">
           <div id="profileContainer" className="flex flex-col md:flex-row bg-[white] w-3/5 m-10 rounded-md shadow-lg shadow-[#EBAF4C] p-4 relative">
-            <img src={profilePic} className="h-72 w-72 rounded-full border-2 border-[#EBAF4C] shadow-lg shadow-stone" />
+            <img src={users[0]?.profilePic} className="h-72 w-72 rounded-full border-2 border-[#EBAF4C] shadow-lg shadow-stone" />
             <div id="detailsContainer" className="flex flex-col ml-5">
-                <h1 className="text-[#EBAF4C] text-3xl font-bold">{firstName} {lastName} <span className="text-[#EBAF4C] text-sm"> {pronouns}</span></h1>
+                <h1 className="text-[#EBAF4C] text-3xl font-bold">{users[0]?.firstName} {users[0]?.lastName} <span className="text-[#EBAF4C] text-sm"> {users[0]?.pronouns}</span></h1>
           <div id="userInfoContainer" className="flex flex-col">
-          <h3 className="text-[#EBAF4C] text-2xl">@{username}</h3>
-            <h3 className="text-[#EBAF4C] text-2xl">{city}, {state}</h3>
-            <h2 className="text-[#EBAF4C] text-2xl"> {birthday}</h2>
-            <p>{bio}</p>
-            <h2 id="rating" className="text-[#EBAF4C] text-2xl">Rating: {rating} </h2>
+          <h3 className="text-[#EBAF4C] text-2xl">@{users[0]?.username}</h3>
+            <h3 className="text-[#EBAF4C] text-2xl">{users[0]?.city}, {users[0]?.state}</h3>
+            <h2 className="text-[#EBAF4C] text-2xl"> {users[0]?.birthday}</h2>
+            <p>{users[0]?.bio}</p>
+            <h2 id="rating" className="text-[#EBAF4C] text-2xl">Rating: {users[0]?.rating} </h2>
             <div className="flex flex-row absolute bottom-0">
               <button id="messageFriend" className="text-white border-2 border-white bg-[#EBAF4C] p-2 hover:bg-white hover:text-[#EBAF4C] hover:border-[#EBAF4C] rounded-md m-2 mb-6">Message</button> 
               <button id="addFriend" 
