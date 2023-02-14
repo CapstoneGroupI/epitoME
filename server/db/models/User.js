31b69f8e0dfa325473c89ef4a2a1fd0cdedb16c6
GMV44 <root@LAPTOP-B6VQPMKU.localdomain>
2023-02-14 17:23:25 -0600

    home fix

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -116,7 +116,7 @@ const User = db.define('user', {
     defaultValue: false
   },
   profilePic: {
-    type: STRING,
+    type: TEXT,
     allowNull: true,
     defaultValue: 'https://t3.ftcdn.net/jpg/01/65/63/94/360_F_165639425_kRh61s497pV7IOPAjwjme1btB8ICkV0L.jpg',
     comment: 'null',
